002e83e6fa810bb3fed588eec16a81b355a3edde
coolaydang <89037815+coolaydang@users.noreply.github.com>
2021-10-23 18:40:37 -0400

    initial

---
+++ example_mods/stages/exampleStage.lua
@@ -0,0 +1,16 @@
+function onCreate()
+	-- background stuff
+	makeLuaSprite('forest','creepyforest',0,25)
+	addLuaSprite('forest',false)
+	setLuaSpriteScrollFactor('forest', 0.5, 0.5);
+	close(true)
+end
+function onBeatHit()
+
+end
+function onStepHit()
+
+end
+function onUpdate()
+
+end
---
+++ example_mods/stages/forest.lua
@@ -0,0 +1,15 @@
+function onCreate()
+	-- background stuff
+	makeLuaSprite('forest','creepyforest',0,25)
+	addLuaSprite('forest',false)
+	close(true)
+end
+function onBeatHit()
+
+end
+function onStepHit()
+
+end
+function onUpdate()
+
+end
