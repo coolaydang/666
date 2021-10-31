d69dfe95e862202fa267d3be0cf0f7b0a3c162aa
coolaydang <89037815+coolaydang@users.noreply.github.com>
2021-10-31 10:53:36 -0400

    initial

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1007,6 +1007,8 @@ class PlayState extends MusicBeatState
 				case 'senpai' | 'roses' | 'thorns':
 					if(daSong == 'roses') FlxG.sound.play(Paths.sound('ANGRY'));
 					schoolIntro(doof);
+				case '666beta':
+					startDialogue(dialogueJson);
 
 				default:
 					startCountdown();
